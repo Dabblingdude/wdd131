b0f332e5717518aaee2b84a12b7296c059f4a620
SNiemand <samniemand1221@gmail.com>
2025-05-01 18:52:30 -0600

    darkest mode

--- mission/script/mission.js
+++ mission/script/mission.js
@@ -11,11 +11,19 @@ function changeTheme() {
 
     if (current == "dark") {
         body.classList.add("dark");
+        body.classList.remove("darkest");
+        logo.setAttribute("src", "images/byui-logo_white.png");
+    }
+
+    else if (current == "darkest") {
+        body.classList.add("darkest");
+        body.classList.remove("dark");
         logo.setAttribute("src", "images/byui-logo_white.png");
     }
 
     else {
         body.classList.remove("dark");
+        body.classList.remove("darkest");
         logo.setAttribute("src", "images/byui-logo_blue.webp");
     }
 }
