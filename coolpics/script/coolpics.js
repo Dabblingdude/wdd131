864a183643a8147f4b000403bf67c24a8e15b808
SNiemand <samniemand1221@gmail.com>
2025-05-13 19:28:53 -0600

    made button work >:(

---
+++ coolpics/script/coolpics.js
@@ -0,0 +1,31 @@
+const menuButton = document.querySelector("button");
+
+menuButton.addEventListener("click", buttonPressed);
+
+window.addEventListener("resize", handleResize);
+
+function buttonPressed() {
+    const menu = document.getElementById('menu');
+    
+    if (menu.classList.contains("hide")) {
+        menu.classList.remove("hide");
+    } 
+    
+    else {
+        menu.classList.add("hide");
+    }
+}
+
+function handleResize() {
+    if (window.innerWidth > 1000) {
+        const menu = document.getElementById('menu');
+
+        menu.classList.remove("hide");
+    }
+
+    else if (window.innerWidth < 1000) {
+        const menu = document.getElementById('menu');
+
+        menu.classList.add("hide");
+    }
+}
